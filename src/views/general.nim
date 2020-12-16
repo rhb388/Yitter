cb19fa3c142c82c81e2607a0535f401650e5a32e
yittb <61764805+yittb@users.noreply.github.com>
2020-12-16 16:42:25 -0600

    Update general.nim

--- src/views/general.nim
+++ src/views/general.nim
@@ -27,7 +27,7 @@ proc renderNavbar*(title, rss: string; req: Request): VNode =
         if rss.len > 0:
           icon "rss-feed", title="RSS Feed", href=rss
         icon "bird", title="Open in Twitter", href=twitterPath
-        a(href="https://liberapay.com/zedeus"): verbatim lp
+        icon "shopping-cart", title="Donate", href=#
         icon "info", title="About", href="/about"
         iconReferer "cog", "/settings", path, title="Preferences"
 
